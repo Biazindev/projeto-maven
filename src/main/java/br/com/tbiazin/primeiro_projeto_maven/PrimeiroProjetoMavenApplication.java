2f32e534e5741c6985cc5381e14e1de6b73bff98
Tiago Biazin <tiago.biazin02@gmail.com>
2024-08-05 21:55:05 -0300

    projeto-maven

---
+++ src/main/java/br/com/tbiazin/primeiro_projeto_maven/PrimeiroProjetoMavenApplication.java
@@ -0,0 +1,13 @@
+package br.com.tbiazin.primeiro_projeto_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tbiazin/primeiro_projeto_maven/PrimeiroProjetoMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tbiazin.primeiro_projeto_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
